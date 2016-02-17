50f2ef4ba33b540fa5a8e324996fa639500765f4
Adrian Kummerlaender <knairda@t-online.de>
2016-02-17 22:06:36 +0100

    Move io utilities into separate compilation unit

---
+++ src/utility/io.cc
@@ -0,0 +1,56 @@
+#include "io.h"
+
+#include <unistd.h>
+#include <dlfcn.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+namespace utility {
+
+FileDescriptorGuard::FileDescriptorGuard(const std::string& path) {
+	this->fd_ = open(path.c_str(), O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
+
+	if ( !this->fd_ ) {
+		this->fd_ = STDERR_FILENO;
+	}
+}
+
+FileDescriptorGuard::~FileDescriptorGuard() {
+	close(this->fd_);
+}
+
+FileDescriptorGuard::operator int() {
+	return this->fd_;
+}
+
+std::string get_file_path(int fd) {
+	char proc_link[20];
+	char file_name[256];
+
+	snprintf(proc_link, sizeof(proc_link), "/proc/self/fd/%d", fd);
+	const ssize_t name_size = readlink(proc_link, file_name, sizeof(file_name));
+
+	if ( name_size > 0 ) {
+		file_name[name_size] = '\0';
+
+		return std::string(file_name);
+	} else {
+		return std::string();
+	}
+}
+
+bool is_regular_file(int fd) {
+	struct stat fd_stat;
+	fstat(fd, &fd_stat);
+
+	return S_ISREG(fd_stat.st_mode);
+}
+
+bool is_regular_file(const char* path) {
+	struct stat fd_stat;
+	lstat(path, &fd_stat);
+
+	return S_ISREG(fd_stat.st_mode);
+}
+
+}
--- src/utility/io.h
+++ src/utility/io.h
@@ -1,67 +1,26 @@
 #ifndef CHANGE_SRC_UTILITY_IO_H_
 #define CHANGE_SRC_UTILITY_IO_H_
 
-#include <unistd.h>
-#include <dlfcn.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-
-#include <cstring>
+#include <string>
 
 namespace utility {
 
 class FileDescriptorGuard {
 	public:
-		FileDescriptorGuard(const std::string& path) {
-			this->fd_ = open(path.c_str(), O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
+		FileDescriptorGuard(const std::string& path);
+		~FileDescriptorGuard();
 
-			if ( !this->fd_ ) {
-				this->fd_ = STDERR_FILENO;
-			}
-		}
-
-		~FileDescriptorGuard() {
-			close(this->fd_);
-		}
-
-		operator int() {
-			return this->fd_;
-		}
+		operator int();
 
 	private:
 		int fd_;
 
 };
 
-std::string get_file_path(int fd) {
-	char proc_link[20];
-	char file_name[256];
+std::string get_file_path(int fd);
 
-	snprintf(proc_link, sizeof(proc_link), "/proc/self/fd/%d", fd);
-	const ssize_t name_size = readlink(proc_link, file_name, sizeof(file_name));
-
-	if ( name_size > 0 ) {
-		file_name[name_size] = '\0';
-
-		return std::string(file_name);
-	} else {
-		return std::string();
-	}
-}
-
-bool is_regular_file(int fd) {
-	struct stat fd_stat;
-	fstat(fd, &fd_stat);
-
-	return S_ISREG(fd_stat.st_mode);
-}
-
-bool is_regular_file(const char* path) {
-	struct stat fd_stat;
-	lstat(path, &fd_stat);
-
-	return S_ISREG(fd_stat.st_mode);
-}
+bool is_regular_file(int fd);
+bool is_regular_file(const char* path);
 
 }
 
